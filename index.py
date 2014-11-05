9921c268136b05d2d816ffc25d51862983018cb3
Noah Bass <bassjnoah@gmail.com>
2014-11-05 09:48:12 -0500

    added png image generation

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, send_file
+from PIL import Image, ImageDraw
+from io import BytesIO
+
 
 app = Flask(__name__)
 
@@ -6,6 +9,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/<width>x<height>/')
+@app.route('/<width>x<height>')
+@app.route('/<width>/')
+@app.route('/<width>')
+def generate_image(width, height='None'):
+    width = int(width)
+
+    if height == 'None':
+        height = width
+    else:
+        height = int(height)
+
+    size = (width, height)             # size of the image to create
+    im = Image.new('RGB', size) # create the image
+    draw = ImageDraw.Draw(im)
+
+    del draw
+
+    img = BytesIO()
+    im.save(img, 'PNG')
+    img.seek(0)
+
+    return send_file(img, mimetype='image/png')
 
 
 @app.route('/<width>x<height>/<svg>/')
